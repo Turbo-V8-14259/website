7225430632065372cf8c32e07e8b7a58a054094c
Sushant Indupuru <sushindupuru@gmail.com>
2025-01-03 18:58:22 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({build: {
+export default defineConfig({
+    site: 'https://testing.ftcturbov8.com', //change after domain change
+    build: {
         inlineStylesheets: 'never'
-    }
+    },
+
+    integrations: [sitemap()]
 });
