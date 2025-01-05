b06751574721700a3ab65ca77ac3f94714b1e4ac
Sushant Indupuru <sushindupuru@gmail.com>
2025-01-05 10:21:19 +0530

    add sponsors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://testing.ftcturbov8.com', //change after domain change
+    base: "/",
     build: {
         inlineStylesheets: 'never'
     },
