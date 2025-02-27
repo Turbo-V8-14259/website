56e00253b411bf902087b369e7bd784ecaaf0562
Sushant Indupuru <sushindupuru@gmail.com>
2025-02-26 21:52:14 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://testing.ftcturbov8.com', //change after domain change
+    site: 'https://ftcturbov8.com', 
     base: "/",
     build: {
         inlineStylesheets: 'never'
